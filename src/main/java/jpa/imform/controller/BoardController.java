313a690eb458f72ffc2834f9df4acfc7076eca22
jihwooon <jihwooon@gmail.com>
2022-03-25 20:56:48 +0900

    feat : plan TODO
    1. request 시 @vada 체크
    2. 회원 가입 하지 못 한 회원은 게시글, 댓글 불가능

--- src/main/java/jpa/imform/controller/BoardController.java
+++ src/main/java/jpa/imform/controller/BoardController.java
@@ -7,22 +7,11 @@
 
 package jpa.imform.controller;
 
-import jpa.imform.domain.Board;
 import jpa.imform.dto.BoardDto;
 import jpa.imform.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.BindingResultUtils;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
