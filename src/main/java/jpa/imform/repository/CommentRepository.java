3e3acd0626d08de56c1d696f9cba4585fe46be00
jihwooon <jihwooon@gmail.com>
2022-03-25 17:55:04 +0900

    fix : test

--- src/main/java/jpa/imform/repository/CommentRepository.java
+++ src/main/java/jpa/imform/repository/CommentRepository.java
@@ -2,7 +2,15 @@ package jpa.imform.repository;
 
 import jpa.imform.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+  @Query(value = "select c from Comment c " +
+      " join fetch c.member m" +
+      " join fetch c.board b")
+  List<Comment> findAllWithCommentThem();
 
 }
