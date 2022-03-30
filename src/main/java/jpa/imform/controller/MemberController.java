2a93b826b8370812da3b2aba73b62f29a575b56f
jihwooon <jihwooon@gmail.com>
2022-03-30 09:18:11 +0900

    refactoring : Add @Requestmapping

--- src/main/java/jpa/imform/controller/MemberController.java
+++ src/main/java/jpa/imform/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,33 +18,34 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
   private final MemberService memberService;
 
-  @GetMapping("member")
+  @GetMapping
   public List<MemberDto.MemberResponse> list() {
     return memberService.getMembers();
   }
 
-  @GetMapping("member/{memberId}")
+  @GetMapping("{memberId}")
   public MemberDto.MemberResponse detail(@PathVariable Long memberId) {
     return MemberDto.MemberResponse.of(memberService.getMember(memberId));
   }
 
-  @PostMapping("member")
+  @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public MemberDto.MemberResponse create(@RequestBody @Valid MemberDto.MemberRequest request) {
     return memberService.createMember(request);
   }
 
-  @PatchMapping("member/{memberId}")
+  @PatchMapping("{memberId}")
   public MemberDto.MemberResponse update(@PathVariable Long memberId, @RequestBody @Valid MemberDto.MemberRequest request) {
     return memberService.updateMember(memberId, request);
   }
 
-  @DeleteMapping("member/{memberId}")
+  @DeleteMapping("{memberId}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void remove(@PathVariable Long memberId) {
     memberService.delete(memberId);
