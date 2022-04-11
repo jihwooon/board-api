46216c4e1823e0542c5aea578811bdd56aeea748
jihwooon <jihwooon@gmail.com>
2022-04-11 14:09:11 +0900

    feat : utils 패키지 생성해서 JwtUtil 생성하기

--- src/main/java/jpa/imform/service/impl/AuthenticationService.java
+++ src/main/java/jpa/imform/service/impl/AuthenticationService.java
@@ -1,21 +1,13 @@
 package jpa.imform.service.impl;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import jpa.imform.utils.JwtUtil;
 import org.springframework.stereotype.Service;
-
-import java.security.Key;
 
 @Service
 public class AuthenticationService {
 
   public String login() {
-    String secret = "12345367890123453678901234536789012";
-    Key key = Keys.hmacShaKeyFor(secret.getBytes());
-
-    return Jwts.builder()
-        .claim("memberId", 1L)
-        .signWith(key)
-        .compact();
+    JwtUtil jwtUtil = new JwtUtil();
+    return jwtUtil.encode(1L);
   }
 }
---
+++ src/main/java/jpa/imform/utils/JwtUtil.java
@@ -0,0 +1,19 @@
+package jpa.imform.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
+public class JwtUtil {
+
+  public String encode(Long memberId) {
+    String secret = "12345367890123453678901234536789012";
+    Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    return Jwts.builder()
+        .claim("memberId", 1L)
+        .signWith(key)
+        .compact();
+  }
+}
