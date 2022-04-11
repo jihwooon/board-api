ffd3efcdd4f84d4f1dd0a98d78e9df5825409a9c
jihwooon <jihwooon@gmail.com>
2022-04-11 10:07:54 +0900

    feat : config 폴더 추가
    springBootController 테스트 이슈로 인한 @EnableJpaAuditing 를 main App 분리작업
    @MockBean을 붙일 바에 별도의 @Configuration 파일로 관리하는게 효율적임

--- src/main/java/jpa/imform/ImformApplication.java
+++ src/main/java/jpa/imform/ImformApplication.java
@@ -2,14 +2,12 @@ package jpa.imform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class ImformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImformApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ImformApplication.class, args);
+  }
 
 }
--- src/test/java/jpa/imform/controller/SessionControllerTest.java
+++ src/test/java/jpa/imform/controller/SessionControllerTest.java
@@ -3,15 +3,12 @@ package jpa.imform.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SessionController.class)
-@MockBean(JpaMetamodelMappingContext.class)
 class SessionControllerTest {
 
   @Autowired
