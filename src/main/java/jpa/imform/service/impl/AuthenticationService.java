28d48a316790b30044e5d9b304dea4bcf9902547
jihwooon <jihwooon@gmail.com>
2022-04-11 11:53:18 +0900

    feat : AuthenticationService Key 추가하기

--- src/main/java/jpa/imform/service/impl/AuthenticationService.java
+++ src/main/java/jpa/imform/service/impl/AuthenticationService.java
@@ -1,11 +1,17 @@
 package jpa.imform.service.impl;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class AuthenticationService {
 
   public String login() {
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
     return "a.b.c";
   }
 }
