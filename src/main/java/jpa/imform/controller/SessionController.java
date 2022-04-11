5190528452efed5334e3e4749edba3ed02048e2e
jihwooon <jihwooon@gmail.com>
2022-04-11 09:43:31 +0900

    test : SessionControllerTest 추가하기
    login test 코드 추가
    JPA metamodel must not be empty! 오류에 대한 이슈 @MockBean(JpaMetamodelMappingContext.class) 추가로 해결

--- src/main/java/jpa/imform/controller/SessionController.java
+++ src/main/java/jpa/imform/controller/SessionController.java
@@ -7,6 +7,20 @@
 
 package jpa.imform.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/session")
 public class SessionController {
-  //login 구현
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public void login() {
+
+  }
+
 }
